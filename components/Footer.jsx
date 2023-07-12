9468b30ccd65994c0bc028ccab2c0ab2527c35d5
Harmeet Singh <harmeetchahal9891@gmail.com>
2023-07-12 22:53:32 +0530

    chnages

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
     <p>All rights reserved</p>
 
     <p className="flex gap-2">
-      <a href="https://github.com/Harmeet135/Electronic-mart" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/Harmeet135/ecommerce" target="_blank" rel="noopener noreferrer">
         <BsGithub className="text-3xl" />
       </a>
-      <a href="https://github.com/Harmeet135/Electronic-mart" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/Harmeet135/ecommerce" target="_blank" rel="noopener noreferrer">
         <MdDomain className="text-3xl" />
       </a>
     </p>
